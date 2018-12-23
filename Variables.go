495186f402bc32a65ac454a5049b716bb2f20d5f
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-12-23 19:32:35 +0600

    variable declaretion with var, type := shorthand

---
+++ Variables.go
@@ -0,0 +1,17 @@
+package main
+import "fmt"
+
+func main(){
+	var initial = "initial value"
+	fmt.Println("Initial value is ", initial)
+
+	var age, height int = 29, 4
+	fmt.Println("age is ", age, ", height is ", height)
+	// update variable
+	var foo int
+	fmt.Println(foo)
+
+	var n string
+	n = "23"
+	fmt.Println(n)
+}
