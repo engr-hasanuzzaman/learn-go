23d74e96c5048f99a57559c81d87a9934f4114e8
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-12-24 23:33:31 +0600

    add for

---
+++ For.go
@@ -0,0 +1,25 @@
+package main
+import "fmt"
+func main()  {
+	const size int = 5
+	// print start piramid using for loop
+	for i := 0; i <= size; i++ {
+		for j := 0; j <= i; j++ {
+			fmt.Printf("*")
+		}
+		fmt.Println("")
+	}
+
+	var inputSize int;
+	fmt.Scanf("Inter pyramid size %d", inputSize)
+	printPyramid(inputSize)
+}
+
+func printPyramid (var size int){
+	for i := 0; i <= size; i++ {
+		for j := 0; j <= i; j++ {
+			fmt.Printf("*")
+		}
+		fmt.Println("")
+	}
+}
