8bcd5aff3be08813ca43e403e2075d2bd32b772c
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-12-25 22:27:08 +0600

    array slice and traversing

--- Slices.go
+++ Slices.go
@@ -18,10 +18,20 @@ func main()  {
 	var str = [3]string{"1st", "2nd", "3rd3rd3rd3rd"}
 	Println("str", str)
 
+	// int array slice using make
 	ages := make([]int, 3)
 	ages[2] = 30
 	ages[0] = 10
 	ages[1] = 20
 	Println(ages)
 	Println("ages[2:3]" , ages[0:1])
+
+	// array slice
+	s1 := [5]int {1, 2, 3, 4, 5}
+	Println("s1 slice", s1[1:3], s1[3:])
+
+	// loop over array
+	for i, value := range s1 {
+		Println("index = ", i, "value is", value) 
+	}
 }
