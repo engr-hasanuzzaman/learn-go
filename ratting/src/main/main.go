677151b11417c65e2aa18b15359c032872f9b4a5
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-12-29 14:31:38 +0600

    rest url with string response

--- ratting/src/main/main.go
+++ ratting/src/main/main.go
@@ -2,20 +2,11 @@ package main
 import (
 	"net/http"
 	"github.com/labstack/echo"
-	"handler"
+	_ "handler"
 )
 
 func index(c echo.Context) error {
 	return c.String(http.StatusOK, "wellcome to echo world")
-}
-
-func showUser(c echo.Context) error {
-	id := c.Param("id")
-	return c.String(http.StatusOK, "User id" + id)
-}
-
-func allUser(c echo.Context) error {
-	return e.Js
 }
 
 func main()  {
@@ -24,15 +15,39 @@ func main()  {
 	e.GET("/", index)
 	// user routes
 	e.POST("/users", createUser)
-	e.GET("/users", handler.Index)
+	e.GET("/users", allUser)
 	e.GET("/users/:id", showUser)
 	e.PUT("/users/:id", updateUser)
 	e.DELETE("/users/:id", deleteUser)
-	e.Logger.Fatal(e.Start(":1234"))
+
+	// 
+	e.Logger.Fatal(e.Start(":5000"))
 }
 
 type User struct{
 	Name string `json:"name"`
 	Title int `json:"title"`
 	Rattings []int `json:"rattings"`
+}
+
+// user route handler
+func showUser(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, "User id " + id + "\n")
+}
+
+func allUser(c echo.Context) error {
+	return c.String(http.StatusOK, "show all user list\n")
+}
+
+func updateUser(c echo.Context) error {
+	return c.String(http.StatusOK, "Update user " + c.Param("id") + "\n")
+}
+
+func createUser(c echo.Context) error {
+	return c.String(http.StatusOK, "New user created\n")
+}
+
+func deleteUser(c echo.Context) error {
+	return c.String(http.StatusOK, "user updated " + c.Param("id") + "\n")
 }
