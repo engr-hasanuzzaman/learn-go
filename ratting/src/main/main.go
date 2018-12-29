f2f85f1c20336fcc8e18505f0fc30b801ce6cd9c
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-12-29 14:08:57 +0600

    creating rest api for user

--- ratting/src/main/main.go
+++ ratting/src/main/main.go
@@ -9,10 +9,30 @@ func index(c echo.Context) error {
 	return c.String(http.StatusOK, "wellcome to echo world")
 }
 
+func showUser(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, "User id" + id)
+}
+
+func allUser(c echo.Context) error {
+	return e.Js
+}
+
 func main()  {
 	e := echo.New()
 
 	e.GET("/", index)
+	// user routes
+	e.POST("/users", createUser)
 	e.GET("/users", handler.Index)
+	e.GET("/users/:id", showUser)
+	e.PUT("/users/:id", updateUser)
+	e.DELETE("/users/:id", deleteUser)
 	e.Logger.Fatal(e.Start(":1234"))
+}
+
+type User struct{
+	Name string `json:"name"`
+	Title int `json:"title"`
+	Rattings []int `json:"rattings"`
 }
