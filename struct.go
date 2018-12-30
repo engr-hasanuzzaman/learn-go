cf437989d2e4622748cc63295278a4168a134f21
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-12-30 20:36:33 +0600

    practice marshallig struc

--- struct.go
+++ struct.go
@@ -1,10 +1,16 @@
 package main
-import . "fmt"
+import(
+	 . "fmt"
+	 "encoding/json"
+	)
 
 func main()  {
 	person := Person{ "hasanuzzaman", "sumon", 29, "male" }
 	Println("person structure is ", person)
 	Println("person full-name is", person.fullName())
+	// test := Test{"sumon", 30}
+	r, _ := json.Marshal(person)
+	Println("marshalling structure", string(r))
 }
 
 type Person struct {
@@ -12,6 +18,11 @@ type Person struct {
 	lastName string
 	age int
 	gender string
+}
+
+type Test struct {
+	Name string `json:"name"`
+	Age int `json:"age"`
 }
 
 func (person *Person) fullName() string{
