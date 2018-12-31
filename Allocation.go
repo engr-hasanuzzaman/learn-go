52bdae71f8f23ac7eb5f7deb2937f12a1286864a
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-12-31 21:31:16 +0600

    check type in different ways

--- Allocation.go
+++ Allocation.go
@@ -1,10 +1,40 @@
 package main
-import . "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main()  {
 	num := new(int) // new create pointer
-	Println("Pointer is", num, "value is", *num)
+	fmt.Println("Pointer is", num, "value is", *num)
 	*num = 1234
-	Println("after assign value, Pointer is", num, "value is", *num)
-	Printf("type of num is %T \n", *num)
-} 
+	fmt.Println("after assign value, Pointer is", num, "value is", *num)
+	fmt.Printf("type of num is %T \n", *num)
+	// type checking 
+	fmt.Println("type of *num 1st way", firstWay(*num)) 
+	fmt.Println("type of *num 2nd way", secondWay(*num)) 
+	fmt.Println("type of *num 2nd way", thirdWay(*num)) 
+}
+
+// three ways to check type
+// N.B: empty interface means any value
+func firstWay(v interface{}) string {
+	return fmt.Sprintf("%T", v)
+}
+
+// using built-in reflect package
+func secondWay(v interface{}) string {
+	return reflect.TypeOf(v).String()
+}
+
+// using type switch
+func thirdWay(v interface{}) string {
+	switch v.(type) {
+	case int:
+		return "int"
+	case string:
+		return "string"
+	default:
+		return "unknown"
+	}
+}
