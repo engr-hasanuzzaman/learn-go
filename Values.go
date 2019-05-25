768a50330d41cbad780056622f748ec1afb4e18f
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2019-05-25 15:21:48 +0600

    use Printf for printing

--- Values.go
+++ Values.go
@@ -2,7 +2,7 @@ package main
 import "fmt"
 func main()  {
 	fmt.Println("Hello " + "Go programming language")
-	fmt.Println("addition: 1 + 2 = ", 1 + 2)
+	fmt.Printf("addition: 1 + 2 = %d \n", 1 + 2)
 	fmt.Println(" 23 / 10 is ", 23 / 10)
 	fmt.Println(" 23 / 10 is ", 23.0 / 10)
 }
