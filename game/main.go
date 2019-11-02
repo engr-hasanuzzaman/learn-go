20d11d4d861d1c00dd7b6a8fb45d57ff1e74af25
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2019-11-03 00:16:15 +0600

    Read csv file and show data on console

--- game/main.go
+++ game/main.go
@@ -1,6 +1,32 @@
 package main
-import . "fmt"
+import(
+	"fmt"
+	"encoding/csv"
+	"io"
+	"os"
+	"log"
+) 
 
 func main()  {
-	Println("This is main file")
+	csv_file, err := os.Open("problems.csv")
+	if err != nil {
+		log.Fatalln("Could not open file ", err)
+	}
+
+	r := csv.NewReader(csv_file)
+
+	for {
+		record, err := r.Read()
+		
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatal("CSV read error")
+		}
+
+		fmt.Printf("Question is %s and answer is %s \n", record[0], record[1])
+	}
+	fmt.Println("This is main file")
 }
