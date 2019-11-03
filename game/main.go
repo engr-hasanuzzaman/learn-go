2bcd470b2f2d65d418c6dbe6ed283ef4a6660a81
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2019-11-04 00:55:41 +0600

    Read all cvs data and prepare on struct

--- game/main.go
+++ game/main.go
@@ -2,7 +2,7 @@ package main
 import(
 	"fmt"
 	"encoding/csv"
-	"io"
+	_ "io"
 	"os"
 	"log"
 ) 
@@ -11,31 +11,53 @@ func main()  {
 	// wait_sec := 15
 	n_correct_answer := 0
 	q_counter := 0
-	user_answer := ""
+	// user_answer := ""
 	csv_file, err := os.Open("problems.csv")
 	if err != nil {
 		log.Fatalln("Could not open file ", err)
 	}
 
 	r := csv.NewReader(csv_file)
-
-	for {
-		record, err := r.Read()
-		
-		if err == io.EOF {
-			break
-		}
-
-		if err != nil {
-			log.Fatal("CSV read error")
-		}
-		fmt.Printf("What is %s = \n", record[0])
-		q_counter += 1
-		fmt.Scanf("%s", &user_answer)
-		if user_answer == record[1] {
-			n_correct_answer += 1
-		}
+	lines, err := r.ReadAll()
+	if err != nil {
+		log.Fatal("error ", err)
 	}
+
+	questions_set := make_problem_set(lines)
+	fmt.Println(questions_set[0].a)
+	// for {
+	// 	record, err := r.Read()
+		
+	// 	if err == io.EOF {
+	// 		break
+	// 	}
+
+	// 	if err != nil {
+	// 		log.Fatal("CSV read error")
+	// 	}
+	// 	fmt.Printf("What is %s = \n", record[0])
+	// 	q_counter += 1
+	// 	fmt.Scanf("%s", &user_answer)
+	// 	if user_answer == record[1] {
+	// 		n_correct_answer += 1
+	// 	}
+	// }
 
 	fmt.Printf("Number of correct answer is %d out or %d \n", n_correct_answer, q_counter)
 }
+
+/* stuct for keeping problem */ 
+type problem struct {
+	q string
+	a string
+}
+
+/* read csv and make array of problems*/
+func make_problem_set(lines [][]string) []problem {
+	res := make([]problem, len(lines))
+	for i, line := range lines {
+		res[i] = problem{line[0], line[1]}
+	}
+	
+	return res
+} 
