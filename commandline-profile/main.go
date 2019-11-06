c6b22087ba2751544b9d904db88d720c5724cb28
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2019-11-07 00:00:38 +0600

    Print all the passing arguments

--- commandline-profile/main.go
+++ commandline-profile/main.go
@@ -11,24 +11,12 @@ import (
 func main() {
   app := cli.NewApp()
 
-  app.Flags = []cli.Flag {
-    cli.StringFlag{
-      Name: "lang",
-      Value: "english",
-      Usage: "language for the greeting",
-    },
-  }
-
   app.Action = func(c *cli.Context) error {
-    name := "Nefertiti"
-    if c.NArg() > 0 {
-      name = c.Args().Get(0)
+    fmt.Println("All the arguments passed as command argument are")
+    for i := 0; i < len(c.Args()); i++ {
+      fmt.Printf("%d th value is %s \n", i, c.Args()[i])  
     }
-    if c.String("lang") == "spanish" {
-      fmt.Println("Hola", name)
-    } else {
-      fmt.Println("Hello", name)
-    }
+    // fmt.Printf("Hello %q", c.Args().Get(0))
     return nil
   }
 
