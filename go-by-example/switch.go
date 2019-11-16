707f23957ce68bc382bc827c74d5188f2d58892c
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2019-11-16 23:17:08 +0600

    No condition switch

--- go-by-example/switch.go
+++ go-by-example/switch.go
@@ -16,4 +16,15 @@ func main()  {
 	default:
 		fmt.Printf("Other")
 	}
+  fmt.Println("")
+
+	// case without condition
+	switch  {
+	case age >= 20 && age < 30:
+		fmt.Println("You are middle aged")
+	case age >= 30:
+		fmt.Printf("You are late 30")
+	default:
+		fmt.Printf("You are default")
+	}
 }
