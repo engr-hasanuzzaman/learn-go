559ca714c9bfd1ab18141ec160fe3ddd7f970423
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2019-11-22 16:09:07 +0600

    read/write env

--- go-by-example/env.go
+++ go-by-example/env.go
@@ -11,4 +11,8 @@ func main() {
 		pair := strings.SplitN(s, "=", 2)
 		fmt.Printf("%s=%s", pair)
 	}
+	fmt.Println()
+	os.Setenv("foo", "this is foo")
+	fmt.Println("env value of foo is ", os.Getenv("foo"))
+	fmt.Println("env value of bar is ", os.Getenv("bar")) // default empty value
 }
