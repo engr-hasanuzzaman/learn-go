df772045fd3a0a41ec5ac6c73a27d19785ba6ad3
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2019-11-24 20:11:54 +0600

    Programm that sometime execure child go routine

--- go-by-example/threading.go
+++ go-by-example/threading.go
@@ -1,7 +1,7 @@
 package main
 import(
 	"fmt"
-	_ "runtime"
+	"runtime"
 )
 
 func say(s string){
@@ -10,7 +10,33 @@ func say(s string){
 	}
 }
 
+func sum(x int) {
+  sum := 0
+  for i := 0; i < x; i++ {
+    sum += i
+  }
+  fmt.Println("sum is", sum)
+}
+
 func main()  {
-	say("hellow")
+	fmt.Println("befeor number of cpu ", runtime.NumCPU())
+	// nCpu := runtime.GOMAXPROCS(4)
+	go say("hello")
 	fmt.Println("World")
+	fmt.Println("World")
+	fmt.Println("World")
+	fmt.Println("World")
+	fmt.Println("World")
+	fmt.Println("World")
+	fmt.Println("World")
+	fmt.Println("World")
+	fmt.Println("World")
+	fmt.Println("World")
+	// fmt.Println("Set Number of CPU is ", nCpu)
+	fmt.Println("After number of cpu ", runtime.NumCPU())
+	go sum(2)
+	go sum(100)
+	go sum(200)
+	for i := 0; i < 100000; i++{
+	}
 }
